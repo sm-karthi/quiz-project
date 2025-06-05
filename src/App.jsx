96e71ad8cddf425c426d0f33fbdbd3ae2659eba9
sm-karthi <karthikeyan24022007@gmail.com>
2025-06-05 16:39:01 +0530

    add logo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ function App() {
 
   return (
     <>
-      <h1>Hello Quiz</h1>
+
+      <div className='w-full bg-gray-50 h-16 border-b border-gray-200 flex items-center justify-between px-5'>
+
+        <img src='../src/assets/images/fav-icon.png' alt='Website logo' className='w-10 h-10'></img>
+
+      </div>
     </>
   )
 }
